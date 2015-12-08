b7358c053c0a1dc784e1f6319491520384dd9447
mchambers623 <mchambers623@gmail.com>
2015-12-08 18:07:29 -0500

    Added some comments

--- ArduinoProgramming/FullControl/FullControl.ino
+++ ArduinoProgramming/FullControl/FullControl.ino
@@ -28,46 +28,47 @@ int FloatVoltage = 750;
 
 void setup() {
 
+    /* Set PWM frequency to 62kHz */
     TCCR0B = (TCCR0B & 0b11111000) | 0b00000001;
 
     
-    // initialize Timer1
-    cli();          // disable global interrupts
-    TCCR1A = 0;     // set entire TCCR1A register to 0
-    TCCR1B = 0;     // same for TCCR1B
- 
-    // set compare match register to desired timer count:
-    OCR1A = 15624;
-    // turn on CTC mode:
-    TCCR1B |= (1 << WGM12);
-    // Set CS10 and CS12 bits for 1024 prescaler:
-    TCCR1B |= (1 << CS10);
-    TCCR1B |= (1 << CS12);
-    // enable timer compare interrupt:
-    TIMSK1 |= (1 << OCIE1A);
-    sei();          // enable global interrupts
+    /* Initialize Timer 1 used for interupt*/
+    cli();                               // disable global interrupts
+    TCCR1A = 0;                          // set entire TCCR1A register to 0
+    TCCR1B = 0;                          // same for TCCR1B
+
+    
+    /* Set up timer to interrupt every second */
+    OCR1A = 15624;                       // set compare match register to desired timer count
+    TCCR1B |= (1 << WGM12);              // turn on CTC mode
+    TCCR1B |= (1 << CS10);               // Set CS10 bits for 1024 prescaler
+    TCCR1B |= (1 << CS12);               // Set CS12 bits for 1024 prescaler
+    TIMSK1 |= (1 << OCIE1A);             // enable timer compare interrupt
+    sei();                               // enable global interrupts
     
     
-    /* Set up pin modes */
+    /* Set up output pin modes */
     pinMode(MainFET, OUTPUT);            // sets the Main Stage FET pin as output
     pinMode(FloatFET, OUTPUT);           // sets the Float Stage FET pin as output
-    pinMode(InitialLED, OUTPUT);         // sets the Isolation FET pin as output
-    pinMode(MainLED, OUTPUT);            // sets the Main Stage FET pin as output
-    pinMode(FloatLED, OUTPUT);           // sets the Float Stage FET pin as output
+    pinMode(InitialLED, OUTPUT);         // sets the Intitial LED pin as output
+    pinMode(MainLED, OUTPUT);            // sets the Main LED pin as output
+    pinMode(FloatLED, OUTPUT);           // sets the Float LED pin as output
     pinMode(PWMOutput, OUTPUT);          // sets PWM pin as output
+
+
+    /* Set up input pin modes */
     pinMode(BatteryInput, INPUT);        // sets battery pin as an input
     pinMode(CurrentCheckInput, INPUT);   // sets current checkpoint as input
 
 
     /* Configure output pin initial conditions */
-    digitalWrite(MainFET, HIGH);   // Main Stage Resistor is not part of circuit
-    digitalWrite(FloatFET, HIGH);  // Float stage resistor is not part of circuit
-    digitalWrite(InitialLED, LOW);    // Battery is not connected to boost converter
-    digitalWrite(MainLED, HIGH);   // Main Stage Resistor is not part of circuit
-    digitalWrite(FloatLED, HIGH);  // Float stage resistor is not part of circuit
+    digitalWrite(MainFET, HIGH);         // Main Stage Resistor is not part of circuit
+    digitalWrite(FloatFET, HIGH);        // Float stage resistor is not part of circuit
+    digitalWrite(InitialLED, LOW);       // Battery is not connected to boost converter
+    digitalWrite(MainLED, HIGH);         // Main Stage Resistor is not part of circuit
+    digitalWrite(FloatLED, HIGH);        // Float stage resistor is not part of circuit
 
-    
-    
+
     /* Set up serial */
     Serial.begin(19200);
     Serial.println("Configured");
@@ -81,10 +82,13 @@ void loop(){
 }
 
 void CheckBatteryVoltage(void){
-    Serial.println("In Check Battery");
+
+    /* Take reading of battery through a 1:3 voltage divider */
     int BatteryVoltage = analogRead(BatteryInput);
-    
+
+    /* If battery voltage is below */
     if(BatteryVoltage < 695){
+      
         /* Set output LED's */
         digitalWrite(InitialLED, HIGH);
         digitalWrite(MainLED, LOW);
@@ -97,7 +101,8 @@ void CheckBatteryVoltage(void){
         /* Set Charging Current */
         Current = InitialCurrent;
 
-        Serial.println("Initial");
+        /* Set stage of charging */
+        Stage = 0;
     }
     
     if(BatteryVoltage >= 695 && BatteryVoltage < 720){
@@ -107,13 +112,14 @@ void CheckBatteryVoltage(void){
         digitalWrite(FloatLED, LOW);
 
         /* Set current setting FET's */
-        digitalWrite(MainFET, LOW);       // Main Stage Resistor is not part of circuit
+        digitalWrite(MainFET, LOW);       // Main Stage Resistor is part of circuit
         digitalWrite(FloatFET, HIGH);     // Float stage resistor is not part of circuit
 
         /* Set Charging Current */
         Current = MainCurrent;
 
-        Serial.println("Main");
+        /* Set stage of charging */
+        Stage = 1;
     }
 
     if(BatteryVoltage >= 720){
@@ -123,18 +129,17 @@ void CheckBatteryVoltage(void){
         digitalWrite(FloatLED, HIGH);
         
         /* Set current setting FET's */
-        digitalWrite(MainFET, LOW);   // Main Stage Resistor is not part of circuit
-        digitalWrite(FloatFET, LOW);  // Float stage resistor is not part of circuit
+        digitalWrite(MainFET, LOW);       // Main Stage Resistor is part of circuit
+        digitalWrite(FloatFET, LOW);      // Float stage resistor is part of circuit
 
         /* Set Charging Current */
         Current = FloatCurrent;
 
-        Serial.println("Float");
+        /* Set stage of charging */
+        Stage = 2;
     }
-    
-    Serial.print("Battery: ");
-    Serial.println(BatteryVoltage);
 }
+
 
 void ChargeBattery(void){
 
@@ -172,7 +177,7 @@ ISR(TIMER1_COMPA_vect){
     Counter++;
     Serial.println(Counter);
    
-    if (Counter >= 1800 && Stage == 0){
+    if (Counter >= 25 && Stage == 0){
         Counter = 0;
         /* Set output LED's */
         digitalWrite(InitialLED, LOW);
@@ -191,7 +196,7 @@ ISR(TIMER1_COMPA_vect){
         Serial.println(Current);
     }
 
-    if (Counter >=7200 && Stage == 1){
+    if (Counter >=25 && Stage == 1){
       /* Set output LED's */
         digitalWrite(InitialLED, LOW);
         digitalWrite(MainLED, LOW);
